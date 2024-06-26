67db736d56cc144b33dcea6fe7dafc0a62395e85
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2024-06-26 04:05:56 -0400

    more pages and some styling
    - added favicons and some other new assets
    - added a way to navigate the multiple pages of search results
    - some nice animations and other css stuff for style
    - new name for the project! we'll see how long this one lasts

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const welcomeContainer = document.getElementById('welcome-container')
 const searchInput = document.getElementById('search-input')
 const searchButton = document.getElementById('search-button')
+const searchSection = document.getElementById('search-section')
 const resultsContainer = document.getElementById('results-container')
 const detailsSection = document.getElementById('details-section')
 const detailsContainer = document.getElementById('details-container')
@@ -10,62 +12,116 @@ let selectionURL = ""
 let selectionTitle = ""
 let selectionCreator = ""
 let selectionDesc = ""
+let resultsTotal = 0
 let pageNum = 1
-
-function displayResults(movies) {
-    resultsContainer.innerHTML = '' // Clear previous results
-
-    if (movies.length === 0) {
-        resultsContainer.innerHTML = '<p>Nothing found.</p>'
-        return
-    }
-
-    movies.forEach(movie => {
-        let movieElement = document.createElement("li")        
-
-        movieElement.innerHTML = `<h3><a href="#top" onclick="getDetails('${movie.identifier}')"><span class="resultNum">${searchResultNum}.</span> ${movie.title}</a></h3><h4>${movie.creator}</h4>`
-        resultsContainer.appendChild(movieElement)
-        searchResultNum++
-    });
-}
+let maxPages = 0
 
 // search upon pressing enter
 searchInput.addEventListener('keyup', async (event) => {
     if (event.key === 'Enter') { // Check if Enter key was pressed
-        const query = searchInput.value
-        const url = `https://archive.org/advancedsearch.php?q=title:(${query})+AND+mediatype:(movies)+AND+format:(MPEG4)&output=json&rows=100&page=${pageNum}&sort=-downloads`
-
+        
+        document.getElementById('search-section').scrollIntoView();
+        welcomeContainer.classList.add("animate")
         searchResultNum = 1
-
-        try {
-            const response = await fetch(url)
-            const data = await response.json()
-
-            displayResults(data.response.docs)
-        } catch (error) {
-            console.error('Error fetching data:', error)
-            // Handle errors (display an error message, etc.)
-        }
+        resultsTotal = 0
+        pageNum = 1
+        maxPages = 0
+    
+        search()
     }
 });
 
 // search upon clicking search button
 searchButton.addEventListener('click', async () => {
-    const query = searchInput.value
-    const url = `https://archive.org/advancedsearch.php?q=title:(${query})+AND+mediatype:(movies)+AND+format:(MPEG4)&output=json&rows=100&page=${pageNum}&sort=-downloads`
-
+    
+    document.getElementById('search-section').scrollIntoView();
+    welcomeContainer.classList.add("animate")
     searchResultNum = 1
+    resultsTotal = 0
+    pageNum = 1
+    maxPages = 0
+
+    search()
+});
+
+async function search() {
+    searchSection.classList.add("show-results")
+
+    const query = searchInput.value
+    const encodedQuery = encodeURIComponent(query)
+    const url = `https://archive.org/advancedsearch.php?q=(title:${encodedQuery} OR creator:${encodedQuery})+AND+mediatype:(movies)+AND+format:(MPEG4)&output=json&rows=100&page=${pageNum}&sort=-downloads`
 
     try {
         const response = await fetch(url)
         const data = await response.json()
 
+        resultsTotal = data.response.numFound
         displayResults(data.response.docs)
     } catch (error) {
+        resultsContainer.innerHTML = '<h2>Nothing found.</h2><h3>Maybe try something else.</h3>'
         console.error('Error fetching data:', error)
         // Handle errors (display an error message, etc.)
     }
-});
+}
+
+function displayResults(movies) {
+    resultsContainer.innerHTML = '' // Clear previous results
+    console.log(movies)
+    if (movies.length === 0) {
+        resultsContainer.innerHTML = '<h2>Nothing found.</h2><h3>Try searching for something else.</h3>'
+        return
+    }
+
+    movies.forEach(movie => {
+        let movieElement = document.createElement("li")        
+        console.log(movie)
+        
+        if (movie.date == undefined) {
+            movieElement.innerHTML = `<h3><a href="#top" onclick="getDetails('${movie.identifier}')"><span class="resultNum">${searchResultNum}.</span> ${movie.title}</a></h3><h4>${movie.creator}</h4>`
+        }
+        else {
+            movieElement.innerHTML = `<h3><a href="#top" onclick="getDetails('${movie.identifier}')"><span class="resultNum">${searchResultNum}.</span> ${movie.title}</a></h3><h4>${movie.creator} | ${movie.date}</h4>`
+        }
+        resultsContainer.appendChild(movieElement)
+        searchResultNum++
+    });
+    // create next page buttons at bottom
+    maxPages = parseInt(resultsTotal / 100) + 1
+
+    let pageControls = document.createElement("div")
+    pageControls.setAttribute("id", "page-controls")
+    if (pageNum == maxPages && pageNum == 1) {}
+    else {
+        pageControls.innerHTML = `<button id="page-back-btn" onclick="pageBack()"><i class="ph ph-caret-left"></i></button><p>Page <span id="page-num">${pageNum}</span> of <span id="max-pages">${maxPages}</span></p><button id="page-next-btn" onclick="pageNext()"><i class="ph ph-caret-right"></i></button>`
+    }
+    resultsContainer.appendChild(pageControls)
+}
+
+// handles pagination
+async function pageBack() {
+    if (pageNum > 1) {
+        if (pageNum == maxPages) {
+            let itemRemainder = resultsTotal % 100
+            searchResultNum -= itemRemainder + 100
+        }
+        else {
+            searchResultNum -= 200
+        }
+
+        pageNum -= 1
+        search()
+        window.scrollTo(0, 0)
+    }
+}
+
+async function pageNext() {
+    if (pageNum < maxPages) {
+        pageNum += 1
+        search()
+        window.scrollTo(0, 0)
+    }
+}
+
 
 // displays the details of the selected result
 function displayDetails() {
@@ -75,7 +131,7 @@ function displayDetails() {
     <div id="video-container"><video id="video-player" src="${selectionURL}" type="video/mp4" controls></video></div>
     <h1>${selectionTitle}</h1>
     <h2>${selectionCreator}</h2>
-    <p>${selectionDesc}</p>
+    <div id="description-container"><p>${selectionDesc}</p></div>
     <div id="file-list-wrapper">
         <h3>Video Files:</h3>
         <ul id="file-list"></ul>
@@ -90,14 +146,15 @@ function displayDetails() {
             fileListItem.innerHTML = `<a href="#top" onclick="changeSource('${selectionObj.files[i].name}')">${selectionObj.files[i].name}</a>`
             fileList.appendChild(fileListItem)
         }
-    }
+    }    
 }
 
 // changes the video players source to the selected file from the file list
 function changeSource(fileName) {
     const videoPlayer = document.getElementById('video-player')
-    
     let fileURL = selectionURL.substring(0, selectionURL.lastIndexOf('/'))
+    
+    fileName = fileName.replace("#", "%23")
     fileURL += '/' + fileName
     videoPlayer.src = fileURL
 }
@@ -118,18 +175,18 @@ async function getDetails(identifier){
         for (let i = 0; i < data.files.length; i++) {
             if (data.files[i].name.endsWith('.mp4') && !data.files[i].name.endsWith('.ia.mp4')) {
                 fileName = data.files[i].name
-                fileName = fileName.replace("#", "%23")
+                fileName = encodeURIComponent(fileName)
                 break
             }
         }
         
-        // use the following link for URL encoding things: https://www.w3schools.com/tags/ref_urlencode.ASP
-        // example: "https://ia802301.us.archive.org/16/items/the-end-of-evangelion-vhs/End%20of%20Evangelion%20(VKLL).mp4"
         selectionObj = data
         selectionURL = `https://${data.d2}${data.dir}/${fileName}`
         selectionTitle = data.metadata.title
         selectionCreator = data.metadata.creator
         selectionDesc = data.metadata.description
+
+        searchSection.classList.add("show-details")
 
         displayDetails()
     } catch (error) {
